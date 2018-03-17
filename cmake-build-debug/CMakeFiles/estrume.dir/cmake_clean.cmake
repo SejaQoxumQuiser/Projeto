3176751db251c3fb27b91a6ac0b3bfe1547bffeb
Everson Otoni <everson.ott@gmail.com>
2018-03-17 08:40:50 -0300

    Add files via upload
    putz

---
+++ cmake-build-debug/CMakeFiles/estrume.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/estrume.dir/main.cpp.o"
+  "estrume.pdb"
+  "estrume"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/estrume.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
